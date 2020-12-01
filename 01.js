a8bf2f087026d25510a73285ac26b5af127530cb
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-01 08:55:24 -0500

    Part2 done

--- 01.js
+++ 01.js
@@ -19,6 +19,26 @@ const part1 = async d => {
 	return null;
 }
 
+const part2 = async d => {
+	const data = d.split('\n').map(e => parseInt(e)).sort((a, b) => a - b);
+	loop:
+	for (let i = 0, li = data.length; i < li; i++) {
+		for (let j = i + 1; j < li; j++) {
+			for (let k = j + 1; k < li; k ++) {
+				const t = data[i] + data[j] + data[k];
+				if (t > 2020) {
+					break;
+				}
+				if (t == 2020) {
+					return data[i] * data[j] * data[k];
+				}
+			}
+		}
+	}
+	return null;
+}
+
 module.exports = {
-	part1
+	part1,
+	part2
 };
