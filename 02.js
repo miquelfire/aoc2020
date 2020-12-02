0dba0f1b47d1164c41f2fa52f6fb2b60164b923e
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-02 10:23:09 -0500

    Day 2 Part 2

--- 02.js
+++ 02.js
@@ -18,11 +18,19 @@ const part1 = async d => {
  * @param {string} d 
  */
 const part2 = async d => {
-	const data = d.split('\n').map(e => e.match(/(\d+)-(\d+) ([a-z]): ([a-z]+)/));
-	return data;
+	let valid = 0;
+	const data = d.split('\n').map(e => e.match(/(\d+)-(\d+) ([a-z]): ([a-z]+)/)).map(e => {
+		const pos1 = e[1] - 1;
+		const pos2 = e[2] - 1;
+		const char = e[3];
+		const pass = e[4];
+		if ((pass[pos1] == char || pass[pos2] == char) && pass[pos1] != pass[pos2]) valid++;
+	});
+	
+	return valid;
 }
 
 module.exports = {
 	part1,
-	//part2
+	part2
 };
