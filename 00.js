17ed53f5bf9be02d9b8440ee9b7d9d9866ca878a
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-02 10:24:57 -0500

    Template for future days

---
+++ 00.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} d 
+ */
+const part1 = async d => {
+	const data = d.split('\n').map(e => parseInt(e));
+	return null;
+}
+
+/**
+ * @param {string} d 
+ */
+const part2 = async d => {
+	const data = d.split('\n').map(e => parseInt(e));
+	return null;
+}
+
+module.exports = {
+	part1,
+	//part2
+};
