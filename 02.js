01a5ebd9a654faeb2f168074a687f4112ddf3feb
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-02 10:18:26 -0500

    Day 2 Part 1

--- 02.js
+++ 02.js
@@ -2,15 +2,23 @@
  * @param {string} d 
  */
 const part1 = async d => {
-	const data = d.split('\n').map(e => parseInt(e)).sort((a, b) => a - b);
-	return data;
+	let valid = 0;
+	const data = d.split('\n').map(e => e.match(/(\d+)-(\d+) ([a-z]): ([a-z]+)/)).map(e => {
+		const min = e[1];
+		const max = e[2];
+		const char = e[3];
+		const pass = e[4].split('').filter(e => e == char);
+		if (pass.length >= min && pass.length <= max) valid++;
+	});
+	
+	return valid;
 }
 
 /**
  * @param {string} d 
  */
 const part2 = async d => {
-	const data = d.split('\n').map(e => parseInt(e)).sort((a, b) => a - b);
+	const data = d.split('\n').map(e => e.match(/(\d+)-(\d+) ([a-z]): ([a-z]+)/));
 	return data;
 }
 
