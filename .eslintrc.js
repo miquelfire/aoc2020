baef9abef72d2aa45d2d8fc7fb9721c9fedb7b37
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-07 19:36:33 -0500

    Install ESLint
    Was wondering why I wasn't getting some errors I'm used to seeing in VS Code

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'ignorePatterns': ['00.js'],
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
