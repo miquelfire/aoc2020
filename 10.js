6cd066552f150383d87735fff3d9d3def16e4a21
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-12 22:36:30 -0500

    Day 10 done

--- 10.js
+++ 10.js
@@ -8,12 +8,12 @@ const part1 = async d => {
 	let diff3 = 1;
 	for (let i = 1; i < data.length; i++) {
 		switch(data[i] - data[i - 1]) {
-		case 1:
-			diff1++;
-			break;
-		case 3:
-			diff3++;
-			break;
+			case 1:
+				diff1++;
+				break;
+			case 3:
+				diff3++;
+				break;
 		}
 	}
 	return diff1 * diff3;
@@ -24,10 +24,23 @@ const part1 = async d => {
  */
 const part2 = async d => {
 	const data = ('0\n' + d).split('\n').map(e => +e).sort((a, b) => a - b);
-	return null;
+	const permutations = [1, 1, 1, 2, 4, 7, 13, 24];
+	let seqLen = 0;
+	let combo = 1;
+	data.forEach((e, i) => {
+		seqLen++;
+		const next = data[i + 1] || e + 3;
+		if (next) {
+			if ((next - e) == 3) {
+				combo *= permutations[seqLen];
+				seqLen = 0;
+			}
+		}
+	});
+	return combo;
 };
 
 module.exports = {
 	part1,
-	//part2
+	part2
 };
