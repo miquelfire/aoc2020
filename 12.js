8b4955394e63c45e7be4fa87b9841afa93867520
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-12 20:48:45 -0500

    Day 12 done

--- 12.js
+++ 12.js
@@ -9,7 +9,6 @@ const part1 = async d => {
 	const WE = 1;
 	const DIR = 2;
 	data.forEach(e => {
-		//const e = [d[0], +(d.slice(1))];
 		switch (e[0]) {
 			case 'F': {
 				switch (pos[DIR]) {
@@ -61,18 +60,80 @@ const part1 = async d => {
 		}
 	});
 	return Math.abs(pos[0]) + Math.abs(pos[1]);
-	//875 too high
 };
 
 /**
  * @param {string} d 
  */
 const part2 = async d => {
-	const data = d.split('\n').map(e => /([NSEWLRF])(\d+)/.exec(e).splice(1));
-	return null;
+	const data = d.split('\n').map(e=> [e[0], +(e.slice(1))]);
+	let waypoint = [-1, 10];
+	const pos = [0, 0];
+	data.forEach(e => {
+		switch(e[0]) {
+			case 'F': {
+				pos[0] += (e[1] * waypoint[0]);
+				pos[1] += (e[1] * waypoint[1]);
+				break;
+			}
+			case 'R': {
+				switch(e[1]) {
+					case 90: {
+						waypoint = [waypoint[1], -waypoint[0]];
+						break;
+					}
+					case 180: {
+						waypoint = [-waypoint[0], -waypoint[1]];
+						break;
+					}
+					case 270: {
+						waypoint = [-waypoint[1], waypoint[0]];
+					}
+				}
+				break;
+			}
+			case 'L': {
+				switch(e[1]) {
+					case 270: {
+						waypoint = [waypoint[1], -waypoint[0]];
+						break;
+					}
+					case 180: {
+						waypoint = [-waypoint[0], -waypoint[1]];
+						break;
+					}
+					case 90: {
+						waypoint = [-waypoint[1], waypoint[0]];
+					}
+				}
+				break;
+			}
+			case 'N': {
+				waypoint[0] -= e[1];
+				break;
+			}
+			case 'S': {
+				waypoint[0] += e[1];
+				break;
+			}
+			case 'W': {
+				waypoint[1] -= e[1];
+				break;
+			}
+			case 'E': {
+				waypoint[1] += e[1];
+				break;
+			}
+			default: {
+				debugger;
+				break;
+			}
+		}
+	});
+	return Math.abs(pos[0]) + Math.abs(pos[1]);
 };
 
 module.exports = {
 	part1,
-	//part2
+	part2
 };
