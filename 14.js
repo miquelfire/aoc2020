8c498fc9ee50aa80be4cef73bc3de97ae4c7228c
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-15 00:33:40 -0500

    Day 14 Part 2

--- 14.js
+++ 14.js
@@ -30,11 +30,46 @@ const part1 = async d => {
  * @param {string} d 
  */
 const part2 = async d => {
-	const data = d.split('\n');
-	return null;
+	const data = d.split('\n').map(e=>e.split(' = '));
+	const mem = new Map();
+	let mask = '';
+	let sum = 0;
+	data.forEach(e=>{
+		if (e[0] == 'mask') {
+			mask = [e[1], (e[1].match(/X/g).length)];
+		}
+		else {
+			let loc = (+(e[0].match(/\d+/)[0])).toString(2).padStart(36, '0').split('');
+			for (let i = 0; i < mask[0].length; i++) {
+				switch (mask[0][i]) {
+					case 'X':
+					case '1':
+						loc[i] = mask[0][i];
+						break;
+				}
+			}
+			for (let i = 0; i < (2 ** mask[1]); i++) {
+				let ib = i.toString(2).padStart(mask[1], '0').split('');
+				let index = 0;
+				const tloc = loc.map((e) => {
+					if (e == 'X') {
+						return ib[index++];
+					}
+					else {
+						return e;
+					}
+				}).join('');
+				mem.set(tloc, +e[1]);
+			}
+		}
+	});
+	mem.forEach(e=>{
+		sum += e;
+	});
+	return sum;
 };
 
 module.exports = {
 	part1,
-	//part2
+	part2
 };
