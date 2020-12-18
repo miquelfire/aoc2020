332a02dd236f782682be4781fde5819f2f094b83
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-18 14:01:40 -0500

    Day 18 Part 2

--- 18.js
+++ 18.js
@@ -4,7 +4,6 @@
  */
 const part1 = async d => {
 	/**
-	 * 
 	 * @param {string} e 
 	 */
 	const calculate = (e) => {
@@ -41,11 +40,36 @@ const part1 = async d => {
  * @param {string} d 
  */
 const part2 = async d => {
-	const data = d.split('\n');
-	return null;
+	/**
+	 * @param {string} e 
+	 */
+	const calculate = (e) => {
+		const parens = /\(([^()]+)\)/;
+		while (parens.test(e)) {
+			e = e.replace(parens, (m, e) => {
+				return calculate(e);
+			});
+		}
+		// parentheses handled, now do the rest
+		const add = /(\d+) \+ (\d+)/g;
+		const multi = /(\d+) \* (\d+)/g;
+		while (add.test(e)) {
+			e = e.replace(add, (m, l, r) => {
+				return (+l) + (+r);
+			});
+		}
+		while (multi.test(e)) {
+			e = e.replace(multi, (m, l, r) => {
+				return (+l) * (+r);
+			});
+		}
+		return +e;
+	};
+	const data = d.split('\n').map(calculate);
+	return data.reduce((p,v)=>p+v, 0);
 };
 
 module.exports = {
 	part1,
-	//part2
+	part2
 };
