a514dc43fae5f57eb5a336404ae44c2180fccaef
Miquel Fire Burns <miquelfire@gmail.com>
2020-12-19 22:35:38 -0500

    Day 19 Part 1

---
+++ 19.js
@@ -0,0 +1,59 @@
+'use strict';
+/**
+ * @param {string} d 
+ */
+const part1 = async d => {
+	const data = d.split('\n\n').map(e=>e.split('\n'));
+	const rules = Array(data[0].length);
+	const rulesToDo = [];
+	data[0].forEach(e=>{
+		let m;
+		if ((m = e.match(/(\d+): "([ab])"/))) {
+			rules[m[1]] =m[2];
+		} else {
+			rulesToDo.push(e);
+		}
+	});
+	while (rulesToDo.length) {
+		/** @type {string} */
+		const rule = rulesToDo.shift(); // Need to be able to push back on the stack later
+		const [ruleNo, r] = rule.split(':').map(e=>e.trim());
+		const ruleParts = r.split(' ');
+		const skip = ruleParts.every(e=>{
+			if (e == '|') {
+				return true;
+			}
+			return rules[e];
+		});
+		if (!skip) {
+			// a Rule hasn't been done yet, just skip for now
+			rulesToDo.push(rule);
+			continue;
+		}
+		rules[ruleNo] = '(' + ruleParts.reduce((p,v)=>{
+			if (v == '|') {
+				return p + v;
+			}
+			return p + rules[v];
+		}, '') + ')';
+	}
+	const re = new RegExp('^' + rules[0] + '$');
+	let sum = 0;
+	data[1].forEach(e=>{
+		if (re.test(e)) sum++;
+	});
+	return sum;
+};
+
+/**
+ * @param {string} d 
+ */
+const part2 = async d => {
+	const data = d.split('\n');
+	return null;
+};
+
+module.exports = {
+	part1,
+	//part2
+};
